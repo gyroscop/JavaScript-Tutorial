2d6581d6666b0d5972a62955710dadc9c68235ec
Dibya Jyoti Sarmah <dibyajyotisarmah531@gmail.com>
2023-08-26 12:34:21 +0000

    Random change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 243421
+let accountName = "Dibya Sarmah"
+var accountEmail = "djs@google.com"
+accountCity = "Kolkata"
+let accountState;
+
+
+console.table([accountId, accountName, accountEmail , accountCity,accountState]);
