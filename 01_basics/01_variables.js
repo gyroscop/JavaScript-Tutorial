39ded13616cf98544655fda7cc95fc5b9f8c1292
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2023-08-26 23:37:00 +0530

    Notes added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,18 @@
-const accountId = 243421
-let accountName = "Dibya Sarmah"
-var accountEmail = "djs@google.com"
-accountCity = "Kolkata"
+const accountId = 243421;
+let accountName = "Dibya Sarmah";
+var accountEmail = "djs@google.com";
+accountCity = "Kolkata";
 let accountState;
 
+/*
+ Not prefer to use 'var'
+ because of the issue in block scope and functional scope
+*/
 
-console.table([accountId, accountName, accountEmail , accountCity,accountState]);
+console.table([
+  accountId,
+  accountName,
+  accountEmail,
+  accountCity,
+  accountState,
+]);
