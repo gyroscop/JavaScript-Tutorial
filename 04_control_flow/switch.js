662cef38caab8177d9fe29d522e053a16030cf84
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2023-09-08 17:17:37 +0530

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,22 @@
+Season = "Vasant";
+
+switch (Season) {
+  case "Vasant":
+    console.log("Season of Spring");
+    break;
+  case "Grisma":
+    console.log("Season of summer");
+    break;
+  case "Boroxha":
+    console.log("Season of monsoon");
+    break;
+  case "Sorot":
+    console.log("Season of Autumn");
+    break;
+  case "Hemanta":
+    console.log("Season of pre-winter");
+    break;
+  case "Xeet":
+    console.log("Season of winter");
+    break;
+}
